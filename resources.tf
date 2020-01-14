fb6c461acdf4fa61535dc7aff96f921ef23a87a0
Mike Martin <mikemartin1090@gmail.com>
2020-01-14 11:02:47 -0500

    add tags

--- resources.tf
+++ resources.tf
@@ -3,6 +3,23 @@ resource "aws_vpc" "environment-example-two" {
     enable_dns_hostnames = "true"
     enable_dns_support = "true"
     tags = {
-        Name = "terraform"
+        Name = "MMartin terraform"
+        Purpose = "MMartin terraform course"
+        DeleteAfter = "2020-01-14"
     }
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)
+  vpc_id = aws_vpc.environment-example-two.id
+  availability_zone = "us-east-2a"
+  tags = {
+        Name = "MMartin terraform"
+        Purpose = "MMartin terraform course"
+        DeleteAfter = "2020-01-14"
+    }
+}
+
+resource "type" "name" {
+  
 }
